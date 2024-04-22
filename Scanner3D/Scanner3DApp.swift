60afc22a748f23ea6befac3af25c08d86472bd11
victoriadepalma <v.depalma>
2024-04-22 15:10:29 -0400

    Initial Commit

---
+++ Scanner3D/Scanner3DApp.swift
@@ -0,0 +1,17 @@
+//
+//  Scanner3DApp.swift
+//  Scanner3D
+//
+//  Created by Victoria De Palma on 4/22/24.
+//
+
+import SwiftUI
+
+@main
+struct Scanner3DApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
